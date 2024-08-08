38d7f3c874fa530649aff7beea3f5b91b6a949e4
Adesola <adesolasolarin@gmail.com>
2024-08-08 09:31:18 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(){
+    var a = Number(num1.value)
+    var b = Number(num2.value)
+    var c = a+b
+    output.innerHTML = "Solution = "+c
+}
+
+function subtract(){
+    var a = Number(num1.value)
+    var b = Number(num2.value)
+    var c = a-b
+    output.innerHTML = "Solution = "+c
+}
+
+function multiply(){
+    var a = Number(num1.value)
+    var b = Number(num2.value)
+    var c = a*b
+    output.innerHTML = "Solution = "+c
+}
+
+function divide(){
+    var a = Number(num1.value)
+    var b = Number(num2.value)
+    var c = a/b
+    output.innerHTML = "Solution = "+c
+}
+
+
